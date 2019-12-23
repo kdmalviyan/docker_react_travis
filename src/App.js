c6797e1e0165a99ed2dbda0c6177a2901ae9a51b
kuldeepsingh <kuldeep.singh@ibm.com>
2019-12-23 16:52:09 +0530

    Msg changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There this msg from nginx.... The value is changeds
+          Yahoo! I am deployed on AWS trhough automated pipeline with travis.
         </p>
         <a
           className="App-link"
